c051357b717c4d6b3637d87583f88a9663553e02
Nathan Chapman <hisbrothernathan@gmail.com>
2014-09-03 22:45:51 -0700

    Updates

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php include_once("contact.html"); ?>
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Our web handlers
+
+$app->get('/', include 'home.html';);
+
+$app->run();
+
+?>
